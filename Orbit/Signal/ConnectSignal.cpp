98d2ae0e38727112f223de6646f50aef6e18bcc9
Robyn C. Hancock <92938217+RobynHancock@users.noreply.github.com>
2021-11-22 16:35:21 +0200

    Added Comments

--- Orbit/Signal/ConnectSignal.cpp
+++ Orbit/Signal/ConnectSignal.cpp
@@ -3,15 +3,15 @@
 
 using namespace std;
 
-ConnectSignal::ConnectSignal(String id)
+ConnectSignal::ConnectSignal(String id) //Constructor for ConnectSignal
 {
 	s->setID(id);
 }
 
-ConnectSignal::~ConnectSignal()
+ConnectSignal::~ConnectSignal() //Destructor for ConnectSignal
 {}
 
-void ConnectSignal::signal()
+void ConnectSignal::signal() //execute function
 {
 	OrbitalControl* OC = new OrbitalControl();
 	OC->connect("Connect");
