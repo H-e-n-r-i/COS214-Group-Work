e3585bc72ddb15d10172ec77ce6f3eb1b5d0b9b5
Robyn C. Hancock <92938217+RobynHancock@users.noreply.github.com>
2021-11-22 15:22:55 +0200

    Removed vector

--- Orbit/StarlinkSatelite.h
+++ Orbit/StarlinkSatelite.h
@@ -3,14 +3,12 @@
 
 //#include "User.h"
 #include <string>
-#include <vector>
 
 class StarlinkSatelite {
     
     private:
         StarlinkSatelite* successor;
         //User* user;
-        vecotr<StarlinkSatelite*> satelites;
         bool online;
 
     public:
@@ -19,8 +17,6 @@ class StarlinkSatelite {
         void sendMessage(std::string, std::string);
         void connect(std::string);
         void disconnect(std::string);
-        void add(StarlinkSatelite* s);
-        void remove();
         void setOnline(bool o);
         bool getOnline();
 };
