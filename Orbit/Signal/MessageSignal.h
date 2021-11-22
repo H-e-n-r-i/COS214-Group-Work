bf959f4ba279a1d89185e242f23d42db1f59b6b3
Robyn C. Hancock <92938217+RobynHancock@users.noreply.github.com>
2021-11-22 14:33:34 +0200

    Update MessageSignal.h

--- Orbit/Signal/MessageSignal.h
+++ Orbit/Signal/MessageSignal.h
@@ -2,9 +2,11 @@
 #define MESSAGESIGNAL_H
 
 #include "Signal.h"
+#include <string>
 
 class MessageSignal : public Signal {
-
+    private:
+        Signal* s;
     public:
         MessageSignal(std::string, std::string);
         ~MessageSignal();
