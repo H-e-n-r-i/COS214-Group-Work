9ba888bc669e3c71d04d80060267bf46ca64f23e
Robyn C. Hancock <92938217+RobynHancock@users.noreply.github.com>
2021-11-22 14:09:47 +0200

    Add files via upload

--- Orbit/StarlinkSatelite.cpp
+++ Orbit/StarlinkSatelite.cpp
@@ -5,11 +5,15 @@ using namespace std;
 
 void StarlinkSatelite::sendMessage(string q, string w)
 {
+	successor->sendMessage(q, w);
 }
 
 void StarlinkSatelite::connect(string q)
-{}
+{
+	successor->connect(q);
+}
 
 void StarlinkSatelite::disconnect(string q)
 {
+	successor->disconnect(q);
 }
